02c295a2d2eac5c554d58dc2c4b89e651bf5698f
cornacchia <pozzan.gabriele@gmail.com>
2020-04-18 15:26:16 +0200

    Fix Expression parser

--- ParseProg.hs
+++ ParseProg.hs
@@ -1,164 +1,14 @@
 module ParseProg where
 import Control.Applicative
 import Parse
-
-data Expr
-  = EInt Int
-  | EBool Bool
-  deriving Show
-
-instance Ord Expr where
-    EInt n1 < EInt n2 = n1 < n2
-    EBool False < EBool True = True
-    b <= c = (b < c) || (b == c)
-    b > c = c < b
-    b >= c = c <= b
-
-instance Eq Expr where
-    EInt n1 == EInt n2 = n1 == n2
-    EBool b1 == EBool b2 = b1 == b2
-    _ == _ = False
-
-exprSum :: Expr -> Expr -> Expr
-exprSum (EInt n1) (EInt n2) = EInt (n1 + n2)
-exprSum _ _ = error "Incorrect Integer SUM operands"
-
-exprDiff :: Expr -> Expr -> Expr
-exprDiff (EInt n1) (EInt n2) = EInt (n1 - n2)
-exprDiff _ _ = error "Incorrect Integer DIFFERENCE operands"
-
-exprMultiply :: Expr -> Expr -> Expr
-exprMultiply (EInt n1) (EInt n2) = EInt (n1 * n2)
-exprMultiply _ _ = error "Incorrect Integer MULTIPLICATION operands"
-
-exprDivide :: Expr -> Expr -> Expr
-exprDivide (EInt n1) (EInt n2) = EInt (div n1 n2)
-exprDivide _ _ = error "Incorrect Integer DIVISION operands"
-
-exprNegate :: Expr -> Expr
-exprNegate (EInt n1) = EInt (negate n1)
-exprNegate _ = error "Incorrect Integer NEGATE operand"
-
-exprAnd :: Expr -> Expr -> Expr
-exprAnd (EBool b1) (EBool b2) = EBool (b1 && b2)
-exprAnd _ _ = error "Incorrect Boolean AND operands"
-
-exprOr :: Expr -> Expr -> Expr
-exprOr (EBool b1) (EBool b2) = EBool (b1 || b2)
-exprOr _ _ = error "Incorrect Boolean OR operands"
-
-exprNot :: Expr -> Expr
-exprNot (EBool b1) = EBool (not b1)
-exprNot _ = error "Incorrect Boolean NOT operands"
-
-parseEqualitySymbol :: Parser String
-parseEqualitySymbol = symbol "!=" <|> symbol "=="
-
-parseComparisonSymbol :: Parser String
-parseComparisonSymbol = symbol "<=" <|> symbol "<" <|> symbol ">=" <|> symbol ">"
-
-parseAdditionSymbol :: Parser String
-parseAdditionSymbol = symbol "+"  <|> symbol "-"
-
-parseMultiplicationSymbol :: Parser String
-parseMultiplicationSymbol = symbol "*" <|> symbol "/"
+import ParseExpression
 
 parseExprs :: Parser [Expr]
-parseExprs = do e <- parseExpr
+parseExprs = do e <- parseExpression
                 do symbol ","
                    es <- parseExprs
                    return (e:es)
                   <|> return [e]
-
-parseExpr :: Parser Expr
-parseExpr = do c1 <- parseSubExpression
-               do symbol <- parseEqualitySymbol
-                  c2 <- parseSubExpression
-                  return (case symbol of
-                          "==" -> EBool (c1 == c2)
-                          "!=" -> EBool (c1 /= c2))
-                 <|> return c1
-
-parseSubExpression :: Parser Expr
-parseSubExpression = parseBooleanExpr <|> parseComparison
-
-parseBooleanExpr :: Parser Expr
-parseBooleanExpr = do t1 <- parseBooleanTerm
-                      do symbol "||"
-                         t2 <- parseBooleanTerm
-                         return (exprOr t1 t2)
-                        <|> return t1
-
-parseBooleanTerm :: Parser Expr
-parseBooleanTerm = do f1 <- parseBooleanFactor
-                      do symbol "&&"
-                         f2 <- parseBooleanFactor
-                         return (exprAnd f1 f2)
-                        <|> return f1
-
-parseBooleanFactor :: Parser Expr
-parseBooleanFactor = parseTrue <|> parseFalse <|> parseNot <|> parseParens
-
-parseTrue :: Parser Expr
-parseTrue = do symbol "True"
-               return (EBool True)
-
-parseFalse :: Parser Expr
-parseFalse = do symbol "False"
-                return (EBool False)
-
-parseNot :: Parser Expr
-parseNot = do symbol "!"
-              f <- parseBooleanFactor
-              return (exprNot f)
-
-parseParens :: Parser Expr
-parseParens = do symbol "("
-                 e <- parseExpr
-                 symbol ")"
-                 return e
-
-parseComparison :: Parser Expr
-parseComparison = do a1 <- parseAddition
-                     do symbol <- parseComparisonSymbol
-                        a2 <- parseAddition
-                        return (case symbol of
-                                "<=" -> EBool (a1 <= a2)
-                                "<" -> EBool (a1 < a2)
-                                ">=" -> EBool (a1 >= a2)
-                                ">" -> EBool (a1 > a2))
-                       <|> return a1
-
-parseAddition :: Parser Expr
-parseAddition = do m1 <- parseMultiplication
-                   do symbol <- parseAdditionSymbol
-                      m2 <- parseMultiplication
-                      return (case symbol of
-                              "+" -> exprSum m1 m2
-                              "-" -> exprDiff m1 m2)
-                     <|> return m1
-
-parseMultiplication :: Parser Expr
-parseMultiplication = do u1 <- parseUnary
-                         do symbol <- parseMultiplicationSymbol
-                            u2 <- parseUnary
-                            return (case symbol of
-                                    "*" -> exprMultiply u1 u2
-                                    "/" -> exprDivide u1 u2)
-                           <|> return u1
-
-parseUnary :: Parser Expr
-parseUnary = do symbol <- symbol "-"
-                u <- parseUnary
-                return (exprNegate u)
-               <|> parsePrimary
-
-parsePrimary :: Parser Expr
-parsePrimary = parseInt <|> parseParens
-
-parseInt :: Parser Expr
-parseInt = do n <- natural
-              return (EInt n)
 
 type Constant = String
 type Channel = String
@@ -226,7 +76,7 @@ parseTauPrefix = do symbol "tau"
 
 parseIfThen :: Parser Process
 parseIfThen = do symbol "if"
-                 b <- parseBooleanExpr
+                 b <- parseBoolExpr
                  symbol "then"
                  proc <- parseProcess
                  return (PIfThen b proc)

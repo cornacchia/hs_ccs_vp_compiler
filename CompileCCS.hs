dc3f6fb48aedcda0040fd124c208288f724159ab
cornacchia <pozzan.gabriele@gmail.com>
2020-05-14 18:10:41 +0200

    Implement compiling logic for relabeling and restriction

--- CompileCCS.hs
+++ CompileCCS.hs
@@ -42,31 +42,51 @@ translateContext :: Context -> String
 translateContext [] = []
 translateContext (x:xs) = show (snd x) ++ "," ++ translateContext xs
 
-compileInputPrefixing :: VP_Process -> Context -> (String, Int) -> Process
-compileInputPrefixing (VP_InputPrefix c e p) ctx d = InputPrefix (c ++ (showExpression e (d:ctx))) (compileProcess p (d:ctx))
+compileInputPrefixing :: VP_Process -> Context -> (String, Int) -> (Process, [(Channel, Int)])
+compileInputPrefixing (VP_InputPrefix c (EInt ie) p) ctx d = (InputPrefix ch cp, ((c, iv):ex))
+                                                     where (cp, ex) = (compileProcess p (d:ctx))
+                                                           iv = evalIntExpr ie (d:ctx)
+                                                           ch = c ++ (show iv)
 
-compileInputPrefixings :: VP_Process -> Context -> Context -> Process
+compileInputPrefixings :: VP_Process -> Context -> Context -> (Process, [(Channel, Int)])
 compileInputPrefixings p ctx [d] = compileInputPrefixing p ctx d
-compileInputPrefixings p ctx (d:ds) = Sum (compileInputPrefixing p ctx d) (compileInputPrefixings p ctx ds)
+compileInputPrefixings p ctx (d:ds) = (Sum cp1 cp2, ex1 ++ ex2)
+                                      where (cp1, ex1) = (compileInputPrefixing p ctx d)
+                                            (cp2, ex2) = (compileInputPrefixings p ctx ds)
 
-compileProcess :: VP_Process -> Context -> Process
-compileProcess VP_Inaction _ = Inaction
-compileProcess (VP_Constant (c, es)) ctx = Const (c ++ (translateExpressions es ctx))
-compileProcess (VP_TauPrefix p) ctx = TauPrefix (compileProcess p ctx)
-compileProcess (VP_Sum p1 p2) ctx = Sum (compileProcess p1 ctx) (compileProcess p2 ctx)
-compileProcess (VP_Parallel p1 p2) ctx = Parallel (compileProcess p1 ctx) (compileProcess p2 ctx)
-compileProcess (VP_OutputPrefix c e p) ctx = OutputPrefix (c ++ (showExpression e ctx)) (compileProcess p ctx)
---compileProcess (VP_Restriction p cs) = Restriction (compileProcess p) [c ++ show n | c <- cs, n <- naturals]
---compileProcess (VP_Relabel p r) = Relabel (compileProcess p) [(c1 ++ show n, c2 ++ show n) | (c1, c2) <- r, n <- naturals]
-compileProcess (VP_IfThen (EBool bexpr) p) ctx = if (evalBoolExpr bexpr ctx) then (compileProcess p ctx) else Inaction
+
+compileProcess :: VP_Process -> Context -> (Process, [(Channel, Int)])
+compileProcess VP_Inaction _ = (Inaction, [])
+compileProcess (VP_Constant (c, es)) ctx = (Const (c ++ (translateExpressions es ctx)), [])
+compileProcess (VP_TauPrefix p) ctx = (TauPrefix cp, ex)
+                                      where (cp, ex) = (compileProcess p ctx)
+compileProcess (VP_Sum p1 p2) ctx = (Sum cp1 cp2, ex1 ++ ex2)
+                                    where (cp1, ex1) = (compileProcess p1 ctx)
+                                          (cp2, ex2) = (compileProcess p2 ctx)
+compileProcess (VP_Parallel p1 p2) ctx = (Parallel cp1 cp2, ex1 ++ ex2)
+                                         where (cp1, ex1) = (compileProcess p1 ctx)
+                                               (cp2, ex2) = (compileProcess p2 ctx)
+compileProcess (VP_OutputPrefix c (EInt ie) p) ctx = (OutputPrefix ch cp, ((c, iv):ex))
+                                             where (cp, ex) = (compileProcess p ctx)
+                                                   iv = evalIntExpr ie ctx
+                                                   ch = c ++ (show iv)
+compileProcess (VP_Restriction p cs) ctx = if ((length ex) > 0) then (rcp, ex) else (cp, ex)
+                                           where (cp, ex) = (compileProcess p ctx)
+                                                 rcp = Restriction cp [c ++ show n | c <- cs, (_, n) <- filter ((==c).fst) ex]
+compileProcess (VP_Relabel p r) ctx = if ((length ex) > 0) then (rcp, ex) else (cp, ex)
+                                      where (cp, ex) = (compileProcess p ctx)
+                                            rcp = Relabel cp [(c1 ++ show n, c2 ++ show n) | (c1, c2) <- r, (_, n) <- filter ((==c2).fst) ex]
+compileProcess (VP_IfThen (EBool bexpr) p) ctx = if bval then (compileProcess p ctx) else (Inaction, [])
+                                                 where bval = (evalBoolExpr bexpr ctx)
 compileProcess (VP_IfThen _ _) _ = error "Non boolean expression used as condition in conditional statement (if then else)"
 compileProcess (VP_InputPrefix x (EInt (EIntVar v)) p) ctx = compileInputPrefixings (VP_InputPrefix x (EInt (EIntVar v)) p) ctx (concat (generateContexts naturals [v]))
-compileProcess (VP_Definition (c, vs) p) ctx = Definition (c ++ (translateContext ctx)) (compileProcess p ctx)
+compileProcess (VP_Definition (c, vs) p) ctx = (Definition (c ++ (translateContext ctx)) cp, ex)
+                                               where (cp, ex) = (compileProcess p ctx)
 
 compiler :: VP_Program -> Program
 compiler [] = []
-compiler ((VP_Definition (c, vs) p):ps) = [compileProcess (VP_Definition (c, vs) p) ctx | ctx <- (generateContexts naturals vs)] ++ (compiler ps)
-compiler (p:ps) = (compileProcess p []) : (compiler ps)
+compiler ((VP_Definition (c, vs) p):ps) = [fst (compileProcess (VP_Definition (c, vs) p) ctx) | ctx <- (generateContexts naturals vs)] ++ (compiler ps)
+compiler (p:ps) = (fst (compileProcess p [])) : (compiler ps)
 
 compileProgram :: [(VP_Program, String)] -> Program
 compileProgram [(p, "")] = compiler p
